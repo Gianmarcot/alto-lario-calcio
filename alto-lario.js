39257b9fc85e82606c36e447414981f3b7ce4425
Gianmarcot <56798756+Gianmarcot@users.noreply.github.com>
2025-07-16 18:00:36 +0200

    Update alto-lario.js

--- alto-lario.js
+++ alto-lario.js
@@ -14,31 +14,3 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// NAVBAR ANIMATION
-const dropdown = document.querySelector('.navbar6_menu-dropdown');
-const navbar = document.querySelector('.navbar6_component');
-
-// Colori iniziali
-const originalBg = window.getComputedStyle(navbar).backgroundColor;
-const originalColor = window.getComputedStyle(navbar).color;
-
-// Nuovo colore
-const newTextColor = 'var(--_primitives---brand--blue-zodiac)';
-
-dropdown.addEventListener('mouseenter', () => {
-  gsap.to(navbar, {
-    backgroundColor: '#ffffff',
-    color: newTextColor,
-    duration: 0.3,
-    ease: 'power1.out'
-  });
-});
-
-dropdown.addEventListener('mouseleave', () => {
-  gsap.to(navbar, {
-    backgroundColor: originalBg,
-    color: originalColor,
-    duration: 0.3,
-    ease: 'power1.out'
-  });
-});
