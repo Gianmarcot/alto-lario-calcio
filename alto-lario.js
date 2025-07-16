a11620e630715f70f6f21f34eceded913f771261
Gianmarcot <56798756+Gianmarcot@users.noreply.github.com>
2025-07-16 17:47:14 +0200

    Update alto-lario.js

--- alto-lario.js
+++ alto-lario.js
@@ -25,3 +25,20 @@ dropdown.addEventListener('mouseleave', () => {
     ease: 'power1.out'
   });
 });
+
+
+// BUTTONS ANIMATION
+document.addEventListener("DOMContentLoaded", () => {
+  const targets = document.querySelectorAll('.b_text');
+
+  targets.forEach(el => {
+    const split = new SplitText(el, {
+      type: "chars",
+      charsClass: "char"
+    });
+
+    split.chars.forEach((char, index) => {
+      char.style.transitionDelay = `${index * 0.01}s`;
+    });
+  });
+});
